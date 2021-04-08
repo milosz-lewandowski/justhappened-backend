30667fa4bb400f8e30750e85a544026339e72dd4
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-04-09 00:50:39 +0200

    Spring Security deletion

--- src/main/java/pl/miloszlewandowski/config/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package pl.miloszlewandowski.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-    public SecurityWebApplicationInitializer() {
-        super(SecurityConfig.class);
-    }
-}
