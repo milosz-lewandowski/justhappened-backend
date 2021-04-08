37ef8792ec8249e92f2d858825c65cbb2337e70f
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-04-08 10:21:35 +0200

    Spring Security Configuration files

---
+++ src/main/java/pl/miloszlewandowski/config/CustomAuthenticationProvider.java
@@ -0,0 +1,31 @@
+package pl.miloszlewandowski.config;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        if ("externaluser".equals(name) && "pass".equals(password)) {       ////TODO: stringi
+            return new UsernamePasswordAuthenticationToken
+                    (name, password, Collections.emptyList());
+        } else {
+            throw new
+                    BadCredentialsException("External system authentication failed");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> auth) {
+        return auth.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
