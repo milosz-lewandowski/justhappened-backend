f23e88acda4a70d57f1c8ec3a8dc4c83eb2d8409
Milosz Lewandowski <milosz.hg@gmail.com>
2021-04-08 05:51:10 +0200

    Starter class & swagger 2 config class

---
+++ src/main/java/pl/miloszlewandowski/JustHappenedPrototypeApplication.java
@@ -0,0 +1,10 @@
+package pl.miloszlewandowski;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class JustHappenedPrototypeApplication {
+
+}
