a8c10bf2249964a039ccc7adc823dbefc0b54b52
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-04-15 17:54:18 +0200

    Controller renamed to home controller

--- src/main/java/pl/miloszlewandowski/controllers/HomeController.java
+++ src/main/java/pl/miloszlewandowski/controllers/HomeController.java
@@ -1,11 +1,19 @@
 package pl.miloszlewandowski.controllers;
 
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 @RestController
-public class PrimalController {
+public class HomeController {
 
     @GetMapping("/")
     public ModelAndView index(){
@@ -20,4 +28,5 @@ public class PrimalController {
         mav.setViewName("justhappened");
         return mav;
     }
+
 }
