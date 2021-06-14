aaaaf9402af581713e196497ae598b6bf75c9a72
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-14 23:20:22 +0200

    Moodcheck.java entity update

--- src/main/java/pl/miloszlewandowski/entities/Moodcheck.java
+++ src/main/java/pl/miloszlewandowski/entities/Moodcheck.java
@@ -18,4 +18,58 @@ public class Moodcheck {
     private Activity activity;
     private Integer mood;
     private Integer selfEsteem;
+
+    public Moodcheck() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public Integer getMood() {
+        return mood;
+    }
+
+    public void setMood(Integer mood) {
+        this.mood = mood;
+    }
+
+    public Integer getSelfEsteem() {
+        return selfEsteem;
+    }
+
+    public void setSelfEsteem(Integer selfEsteem) {
+        this.selfEsteem = selfEsteem;
+    }
+
+    @Override
+    public String toString() {
+        return "Moodcheck{" +
+                "id=" + id +
+                ", dateTime=" + dateTime +
+                ", activity=" + activity +
+                ", mood=" + mood +
+                ", selfEsteem=" + selfEsteem +
+                '}';
+    }
 }
