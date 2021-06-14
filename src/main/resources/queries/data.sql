a6dc41859c10338e1b66d6ab638d1a5154f82c16
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-14 23:19:00 +0200

    data.sql add moddcheck, and activities insert queries

--- src/main/resources/queries/data.sql
+++ src/main/resources/queries/data.sql
@@ -11,3 +11,19 @@ values
 (1, 'p1', 1),
 (2, 'p2', 2),
 (3, 'p3', 2);
+
+insert into
+    activities (id, activity_name, patient_id)
+values
+(1, 'activity1', 1),
+(2, 'activity2', 2),
+(3, 'activity3', 2),
+(4, 'activity4', 3),
+(5, 'activity5', 2);
+
+insert into
+    moodchecks (id, date_time, mood, self_esteem, activity_id)
+values
+(1, now(), 6, 7, 1),
+(2, now(), 4, 8, 2),
+(3, now(), 5, 5, 2);
