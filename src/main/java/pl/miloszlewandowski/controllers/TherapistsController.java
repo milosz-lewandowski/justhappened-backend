b67eec3c784ee133a49024146a6622c70c6a5dab
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-15 02:01:55 +0200

    TherapistsController CRUD implemented

--- src/main/java/pl/miloszlewandowski/controllers/TherapistsController.java
+++ src/main/java/pl/miloszlewandowski/controllers/TherapistsController.java
@@ -1,9 +1,7 @@
 package pl.miloszlewandowski.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import pl.miloszlewandowski.entities.Patient;
+import org.springframework.web.bind.annotation.*;
 import pl.miloszlewandowski.entities.Therapist;
 import pl.miloszlewandowski.repositories.TherapistRepository;
 
@@ -21,14 +19,35 @@ public class TherapistsController {
         this.therapistRepository = therapistRepository;
     }
 
-
-
     @GetMapping("/therapists")
-    public Map<String, List<Therapist>> therapistsList(){
+    public ResponseEntity<Map<String, List<Therapist>>> therapistsList(){
         List<Therapist> therapists = new ArrayList<>(therapistRepository.findAll());
-        therapists.forEach(therapist -> therapist.setPatients(null));
         Map<String, List<Therapist>> responseNData = new HashMap();
         responseNData.put("therapists", therapists);
-        return responseNData;
+        return ResponseEntity.ok(responseNData);
+    }
+
+    @GetMapping("/therapists/{id}")
+    public ResponseEntity<Map<String, Therapist>> getTherapist(@PathVariable("id")Long id){
+        Map<String, Therapist> map = new HashMap<>();
+        map.put("therapist", therapistRepository.getById(id));
+        return ResponseEntity.ok(map);
+    }
+
+    @PostMapping("therapists")
+    public void saveTherapist(@RequestBody Therapist therapist){
+        therapistRepository.save(therapist);
+    }
+
+    @PutMapping("therapists/{id}")
+    public void updateTherapist(@RequestBody Therapist therapist){
+//        therapistRepository.getById(therapist.getId());
+        therapistRepository.save(therapist);
+    }
+
+    @DeleteMapping("/therapists/{id}")
+    public void deleteTherapist(@PathVariable("id") Long id){
+        therapistRepository.deleteById(id);
     }
 }
+
