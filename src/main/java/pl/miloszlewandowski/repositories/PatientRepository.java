bb8716249880978b927fa9a6753a69046df94cc7
Milosz Lewandowski <milosz.hg@gmail.com>
2021-06-15 02:18:13 +0200

    add getPatientsByTherapistId method

--- src/main/java/pl/miloszlewandowski/repositories/PatientRepository.java
+++ src/main/java/pl/miloszlewandowski/repositories/PatientRepository.java
@@ -2,7 +2,8 @@ package pl.miloszlewandowski.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.miloszlewandowski.entities.Patient;
+import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-
+    List<Patient> getPatientsByTherapistId(long therapistId);
 }
