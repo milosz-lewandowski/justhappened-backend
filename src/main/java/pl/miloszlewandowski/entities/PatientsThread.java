7fc374d8f26c86a53753d3fddebfdfe45a70885b
Milosz Lewandowski <milosz.hg@gmail.com>
2021-07-02 04:45:55 +0200

    PatientsThread.java

--- src/main/java/pl/miloszlewandowski/entities/PatientsThread.java
+++ src/main/java/pl/miloszlewandowski/entities/PatientsThread.java
@@ -1,14 +1,45 @@
 package pl.miloszlewandowski.entities;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "threads")
-public class PatientsThread implements Serializable {
+public class PatientsThread  {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long PatientThreadId;
     private String name;
+    @ManyToOne
+    private Record record;
+
+    public Long getPatientThreadId() {
+        return PatientThreadId;
+    }
+
+    public void setPatientThreadId(Long patientThreadId) {
+        PatientThreadId = patientThreadId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Record getRecord() {
+        return record;
+    }
+
+    public void setRecord(Record record) {
+        this.record = record;
+    }
+
+    public PatientsThread() {
+    }
 }
