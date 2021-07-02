d5c3afdb74858724633903eb7b766cdee5bb684e
Milosz Lewandowski <milosz.hg@gmail.com>
2021-07-02 04:44:39 +0200

    Patient.java

--- src/main/java/pl/miloszlewandowski/entities/Patient.java
+++ src/main/java/pl/miloszlewandowski/entities/Patient.java
@@ -1,31 +1,36 @@
 package pl.miloszlewandowski.entities;
 
 import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Objects;
+import java.util.List;
 
 @Entity
 @Table(name = "patients")
-public class Patient implements Serializable {
+public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long patientId;
     private String name;
+    @OneToMany
+    @JoinColumn(name = "activity_id")
+    List<Activity> activities;
     @ManyToOne
-    @JoinColumn(name = "therapists.id")
     private Therapist therapist;
 
-    public Patient() {
-
+    public List<Activity> getActivities() {
+        return activities;
     }
 
-    public Long getId() {
-        return id;
+    public void setActivities(List<Activity> activities) {
+        this.activities = activities;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
     }
 
     public String getName() {
@@ -44,25 +49,6 @@ public class Patient implements Serializable {
         this.therapist = therapist;
     }
 
-    @Override
-    public String toString() {
-        return "Patient{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", therapist=" + therapist +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Patient patient = (Patient) o;
-        return Objects.equals(id, patient.id) && Objects.equals(name, patient.name) && Objects.equals(therapist, patient.therapist);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, therapist);
+    public Patient() {
     }
 }
