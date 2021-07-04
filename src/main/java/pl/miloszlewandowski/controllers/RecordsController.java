b575ac69673902d4c5157bbe40b26b22cc94a828
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-07-04 23:57:02 +0200

    RecordsController.java
    Signed-off-by: Miłosz Lewandowski <milosz.hg@gmail.com>

--- src/main/java/pl/miloszlewandowski/controllers/RecordsController.java
+++ src/main/java/pl/miloszlewandowski/controllers/RecordsController.java
@@ -1,6 +1,5 @@
 package pl.miloszlewandowski.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
