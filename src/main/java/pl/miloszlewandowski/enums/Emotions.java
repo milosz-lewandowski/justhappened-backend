da556a081a0c29c1d1a0c82e773bfba6b3227dd4
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-07-06 21:16:57 +0200

    Emotions as enum
    Signed-off-by: Miłosz Lewandowski <milosz.hg@gmail.com>

---
+++ src/main/java/pl/miloszlewandowski/enums/Emotions.java
@@ -0,0 +1,12 @@
+package pl.miloszlewandowski.enums;
+
+public enum Emotions {
+    FURIOUS(EmotionsGroup.ANGRY),
+    BORED(EmotionsGroup.SAD),
+    SURPRISED(EmotionsGroup.SCARED),
+    ;
+
+    Emotions(EmotionsGroup group) {
+
+    }
+}
