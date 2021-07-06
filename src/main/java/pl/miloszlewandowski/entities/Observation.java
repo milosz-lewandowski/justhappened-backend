951fc3c9b6db81d13988686f992a7fcbfcbaf461
Milosz Lewandowski <milosz.hg@gmail.com>
2021-07-06 21:18:58 +0200

    Observation.java
    Signed-off-by: Miłosz Lewandowski <milosz.hg@gmail.com>

---
+++ src/main/java/pl/miloszlewandowski/entities/Observation.java
@@ -0,0 +1,59 @@
+package pl.miloszlewandowski.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Observation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long observationId;
+    private String thought;
+    private String situation;
+    private String effect;
+    @ManyToOne
+    private PatientsThread thread;
+
+    public Observation() {
+    }
+
+    public String getThought() {
+        return thought;
+    }
+
+    public void setThought(String thought) {
+        this.thought = thought;
+    }
+
+    public String getSituation() {
+        return situation;
+    }
+
+    public void setSituation(String situation) {
+        this.situation = situation;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
+
+    public void setEffect(String effect) {
+        this.effect = effect;
+    }
+
+    public PatientsThread getThread() {
+        return thread;
+    }
+
+    public void setThread(PatientsThread thread) {
+        this.thread = thread;
+    }
+
+    public Long getObservationId() {
+        return observationId;
+    }
+
+    public void setObservationId(Long observationId) {
+        this.observationId = observationId;
+    }
+}
