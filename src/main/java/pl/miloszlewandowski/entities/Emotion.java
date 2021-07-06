76a9edbfbcc0c7dcead95607de9f106131e157cf
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-07-06 21:15:01 +0200

    Emotion entity
    Signed-off-by: Miłosz Lewandowski <milosz.hg@gmail.com>

---
+++ src/main/java/pl/miloszlewandowski/entities/Emotion.java
@@ -0,0 +1,42 @@
+package pl.miloszlewandowski.entities;
+
+import pl.miloszlewandowski.enums.EmotionsGroup;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "emotions")
+public class Emotion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long emotionId;
+    private String name;
+    private EmotionsGroup group;
+
+    public Emotion() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EmotionsGroup getGroup() {
+        return group;
+    }
+
+    public void setGroup(EmotionsGroup group) {
+        this.group = group;
+    }
+
+    public Long getEmotionId() {
+        return emotionId;
+    }
+
+    public void setEmotionId(Long emotionId) {
+        this.emotionId = emotionId;
+    }
+}
