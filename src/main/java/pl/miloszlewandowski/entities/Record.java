57c4481f9d51799e518876f6764d585bb752d3d8
Milosz Lewandowski <milosz.hg@gmail.com>
2021-07-06 21:19:47 +0200

    Record entity update
    Signed-off-by: Miłosz Lewandowski <milosz.hg@gmail.com>

--- src/main/java/pl/miloszlewandowski/entities/Record.java
+++ src/main/java/pl/miloszlewandowski/entities/Record.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "records")
@@ -17,22 +18,18 @@ public class Record {
     private LocalDateTime dateTime;
     @ManyToOne(fetch = FetchType.LAZY)
     private Patient patient;
-    private boolean hasMoodcheck;
-
-    public Moodcheck getMoodcheck() {
-        return moodcheck;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public void setMoodcheck(Moodcheck moodcheck) {
-        this.moodcheck = moodcheck;
-    }
-
+    private boolean hasMoodCheck;
+//    private boolean hasTargetCheck;
+    private boolean hasObservation;
+    private boolean hasEmotions;
     @OneToOne
-    private Moodcheck moodcheck;
+    private MoodCheck moodcheck;
+//    @OneToOne
+//    private TargetCheck targetCheck;
+    @OneToOne
+    private Observation observation;
+    @OneToMany
+    private List<Emotion> emotions;
 
     public Long getRecordId() {
         return RecordId;
@@ -46,6 +43,10 @@ public class Record {
         return dateTime;
     }
 
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
     public Patient getPatient() {
         return patient;
     }
@@ -54,12 +55,52 @@ public class Record {
         this.patient = patient;
     }
 
-    public boolean isHasMoodcheck() {
-        return hasMoodcheck;
+    public boolean isHasMoodCheck() {
+        return hasMoodCheck;
     }
 
-    public void setHasMoodcheck(boolean hasMoodcheck) {
-        this.hasMoodcheck = hasMoodcheck;
+    public void setHasMoodCheck(boolean hasMoodCheck) {
+        this.hasMoodCheck = hasMoodCheck;
+    }
+
+    public boolean isHasObservation() {
+        return hasObservation;
+    }
+
+    public void setHasObservation(boolean hasObservation) {
+        this.hasObservation = hasObservation;
+    }
+
+    public boolean isHasEmotions() {
+        return hasEmotions;
+    }
+
+    public void setHasEmotions(boolean hasEmotions) {
+        this.hasEmotions = hasEmotions;
+    }
+
+    public MoodCheck getMoodcheck() {
+        return moodcheck;
+    }
+
+    public void setMoodcheck(MoodCheck moodcheck) {
+        this.moodcheck = moodcheck;
+    }
+
+    public Observation getObservation() {
+        return observation;
+    }
+
+    public void setObservation(Observation observation) {
+        this.observation = observation;
+    }
+
+    public List<Emotion> getEmotions() {
+        return emotions;
+    }
+
+    public void setEmotions(List<Emotion> emotions) {
+        this.emotions = emotions;
     }
 
     public Record() {
